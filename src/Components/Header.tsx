cac88903f842d97a39338d189670c6652b012a27
Gyuha Park <gyuhapark11@gmail.com>
2023-04-09 23:12:04 +0900

    Ch. 2-6-2

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,19 @@
-import Nav from "./Nav";
 import DrawerMenu from "./DrawerMenu";
 
 export default function Header() {
   return (
     <header className="header">
-      <Nav />
+      <ul className="header__list">
+        <li className="header__list-item">
+          <a href="#LINK1">LINK1</a>
+        </li>
+        <li className="header__list-item">
+          <a href="#LINK2">LINK2</a>
+        </li>
+        <li className="header__list-item">
+          <a href="#LINK3">LINK3</a>
+        </li>
+      </ul>
       <DrawerMenu />
     </header>
   );
